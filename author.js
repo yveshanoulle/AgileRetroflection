d80b431ada2e34957faaaecae575ae8cc262a773
leider <derleider@web.de>
2013-03-22 02:39:56 +0100

    now you can send feedback emails

--- author.js
+++ author.js
@@ -20,13 +20,27 @@ function Author(name) {
         };
     }
 
+    function createCorrection(question) {
+        return '<a data-role="button" data-icon="edit" data-iconpos="notext" class="mailbutton" href=\'mailto:Yves@PairCoaching.net' +
+            '?subject=Retroflection corrected question' +
+            '&body=' + escape("I have a proposal on improving the spelling of retroflection #" + question.number +
+            ': \n' + '"' + question.question + '"' + ' by ' + question.author) +
+            encodeURIComponent('\n\nI would write it as follows:\n\n\n---\n' +
+                'This retroflection was originally twittered by @retroflection' +
+                '\nand is sent via the retroflection app available at http://retroflection.org') + '\'>' +
+            'correct<br>spelling</a>';
+    }
+
     if (typeof(this.asListItem) == 'undefined') //guarantees one time prototyping
     {
         Author.prototype.asListItem = function () {
             var ul = "<ul data-role='listview'>";
             for (questionIndex in this.questions) {
                 var question = this.questions[questionIndex];
-                ul += "<li><small>" + question.question + "</small></li>";
+                ul += "<li><small>" + question.question + "</small>" +
+                    "<p class='ui-li-aside'>" +
+                    createCorrection(question) + "</p>" +
+                    "</li>";
             }
             ul += "</ul>";
             return "<div data-role='collapsible'><h3>" + this.name + "  (" + this.questions.length + ")</h3>" + ul + "</div>";
--- retroflection.js
+++ retroflection.js
@@ -1,75 +1,83 @@
 var persistence = {};
-(function() {
-	this.get_id = function() {
-		if (!localStorage.getItem("id")) {
-			localStorage.setItem("id", 0);
-		}
-		return Math.floor(localStorage.getItem("id"))
-	}
+(function () {
+    this.get_id = function () {
+        if (!localStorage.getItem("id")) {
+            localStorage.setItem("id", 0);
+        }
+        return Math.floor(localStorage.getItem("id"))
+    }
 
-	this.set_id = function(token) {
-		localStorage.setItem("id", token)
-	}
+    this.set_id = function (token) {
+        localStorage.setItem("id", token)
+    }
 }).apply(persistence)
 
 var retroflection = {};
-(function() {
+(function () {
 
-	this.currentQuestion = function() {
-		return questions[Math.floor(Math.random() * questions.length)];
-	}
+    this.currentQuestion = function () {
+        return questions[Math.floor(Math.random() * questions.length)];
+    }
 
 }).apply(retroflection)
 
 var display = {};
-(function() {
-    var linkToTwitter = function(name) {
+(function () {
+    var linkToTwitter = function (name) {
         if (name.charAt(0) == "@") {
             return "<a href='http://twitter.com/" + name.substr(1) + "'>" + name + "</a>";
         }
         return name;
     }
 
-    this.show = function(number) {
-		var current = retroflection.currentQuestion();
-		$("#question" + number).html(current.question);
-		$("#author" + number).html(linkToTwitter(current.author) + " (#" + current.number + ")");
-	}
+    var createEmail = function (question) {
+        return '<a href=\'mailto:?subject=Retroflection #' + question.number +
+            '&body=' + escape('"' + question.question + '"' + ' by ' + question.author) +
+            encodeURIComponent('\n\n---\nThis retroflection was originally twittered by @retroflection' +
+                '\nand is sent via the retroflection app available at http://retroflection.org') + '\'>' +
+            'mail it</a>';
+    }
 
-	this.bindEvents = function() {
-		bindButtonsNumbered(1,2);
-		bindButtonsNumbered(2,1);
-		bindGesturesForPage(1);
-		bindGesturesForPage(2);
-	}
+    this.show = function (number) {
+        var current = retroflection.currentQuestion();
+        $("#question" + number).html(current.question);
+        $("#author" + number).html(linkToTwitter(current.author) + " (#" + current.number + ")");
+        $("#emaillink" + number).html(createEmail(current));
+    }
 
-	var bindButtonsNumbered = function(buttonNumber, number) {
-		$("#page" + number).bind("pagebeforeshow", function() {
+    this.bindEvents = function () {
+        bindButtonsNumbered(1, 2);
+        bindButtonsNumbered(2, 1);
+        bindGesturesForPage(1);
+        bindGesturesForPage(2);
+    }
+
+    var bindButtonsNumbered = function (buttonNumber, number) {
+        $("#page" + number).bind("pagebeforeshow", function () {
             display.show(number);
-		})
-	}
-	
-	var bindGesturesForPage = function(pageNumber) {
-		$("#content" + pageNumber).bind ("swipedown", function (event)
-		{
-			event.stopImmediatePropagation();
-			$("#random" + pageNumber).trigger("click");
-			return false;
-		});
-	}
+        })
+    }
+
+    var bindGesturesForPage = function (pageNumber) {
+        $("#content" + pageNumber).bind("swipedown", function (event) {
+            event.stopImmediatePropagation();
+            $("#random" + pageNumber).trigger("click");
+            return false;
+        });
+    }
 
 }).apply(display)
 
-jQuery.fn.retroflection = function() {
-	display.show(1);
-	display.show(2);
-	display.bindEvents();
+jQuery.fn.retroflection = function () {
+    display.show(1);
+    display.show(2);
+    display.bindEvents();
 }
 
-jQuery.fn.contrib = function() {
+jQuery.fn.contrib = function () {
     var map = new Authors(questions).internal;
-	for (index in map) {
+    for (index in map) {
         var author = map[index];
         $("#authors").append(author.asListItem());
-	}
+    }
 }
