e41fdcd8157ef77f2f30747a7302fbb545e99c8a
leider <derleider@web.de>
2013-03-29 00:38:47 +0100

    fixed db access

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ var
   mongo = require('mongodb'),
   mongoUri = process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    'mongodb://localhost/test?safe=true';
-  adminName = process.env.ADMIN_NAME || 'admin';
-  adminPass = process.env.ADMIN_PASS || '12345';
+    'mongodb://localhost/test?safe=true',
+  adminName = process.env.ADMIN_NAME || 'admin',
+  adminPass = process.env.ADMIN_PASS || '12345',
+  Db = mongo.Db.connect(mongoUri, {noOpen: true}, function (err, db) {
+  });
 
 
 app.configure(function () {
@@ -28,65 +30,44 @@ var basicAuth = express.basicAuth(function (username, password) {
   return (username == adminName && password == adminPass);
 }, 'Restrict area, please identify');
 
-var loadFile = function (file, cb) {
-  fs.readFile(file, 'utf8', function (err, code) {
-    if (err) {
-      return cb("error loading file " + err);
-    }
-    try {
-      cb(null, JSON.parse(code));
-    }
-    catch (e) {
-      cb("Error parsing " + file + ": " + e);
-    }
-  });
-};
-
 function getQuestions(callback) {
-  mongo.Db.connect(mongoUri, function (err, db) {
-    if (err) {
-      return loadFile('static/questions-all.json', function (error, result) {
-        if (error) {
-          callback(error);
-        }
-        callback(null, result);
-      });
-    }
+  Db.open(function (err, db) {
     var test = db.collection('test');
     test.find().toArray(function (error, result) {
+      db.close();
       if (error) {
-        next(error);
+        return callback(error);
       }
       result.sort(function (a, b) {
         return a.id.localeCompare(b.id);
       })
       callback(null, result);
-    })
+    });
   });
 }
 
 function getQuestion(id, callback) {
-  mongo.Db.connect(mongoUri, function (err, db) {
+  Db.open(function (err, db) {
     var test = db.collection('test');
     test.find({id: id}).toArray(function (error, result) {
+      db.close();
       if (error) {
-        next(error);
+        return callback(error);
       }
       callback(null, result[0]);
-    })
+    });
   });
 }
 
-function saveQuestion(req, res) {
-  var question = req.body;
-  mongo.Db.connect(mongoUri, function (err, db) {
+function saveQuestion(question, callback) {
+  Db.open(function (err, db) {
     var test = db.collection('test');
-    test.update({id: question.id}, {$set: {question: question.question}}, {w: 1}, function (err, result) {
-      if (result === 1) {
-        res.render('updated', {question: question});
-      } else {
-        res.redirect('/questions/' + question.id + '/edit');
+    test.update({id: question.id}, {$set: {question: question.question}}, {w: 1}, function (error, result) {
+      db.close();
+      if (error) {
+        return callback(error);
       }
+      callback(null, result);
     });
   });
 }
@@ -96,29 +77,45 @@ app.get('/questions.json', function (req, res, next) {
     if (err) {
       next(err);
     }
-//    res.writeHead(200, { 'Content-Type': 'application/javascript' });
     res.send('questions = ' + JSON.stringify(result));
   })
 });
 
 app.get('/questions/', basicAuth, function (req, res, next) {
   getQuestions(function (err, result) {
+    if (err) {
+      next(err);
+    }
     res.render('index', {questions: result});
   })
 });
 
-app.get('/questions', basicAuth, function (req, res, next) {
+app.get('/questions', basicAuth, function (req, res) {
   res.redirect('/questions/');
 });
 
-app.get('/questions/:id/edit', basicAuth, function (req, res) {
+app.get('/questions/:id/edit', basicAuth, function (req, res, next) {
   getQuestion(req.params.id, function (err, result) {
+    if (err) {
+      next(err);
+    }
     res.render('edit', {question: result});
   })
 });
 
-app.post('/questions/:id/submit', basicAuth, function (req, res) {
-  saveQuestion(req, res);
+app.post('/questions/:id/submit', basicAuth, function (req, res, next) {
+  var question = req.body;
+  saveQuestion(question, function (err, result) {
+    if (err) {
+      return next(err);
+    }
+    if (result === 1) {
+      res.render('updated', {question: question});
+    } else {
+      res.redirect('/questions/' + question.id + '/edit');
+    }
+  });
+
 });
 
 var http = require('http');
