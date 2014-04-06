d9072baad57ea2c6fc481f783e366110ee6d387e
leider <derleider@web.de>
2014-04-06 17:45:19 +0200

    tests should reflect the actual layout

--- browser-test/author-test.js
+++ browser-test/author-test.js
@@ -4,13 +4,13 @@ describe('author Yves', function () {
   var authorName = '@yveshanoulle';
   var author = new Author(authorName);
 
-  beforeEach(function(){
+  beforeEach(function () {
     author = new Author(authorName);
     for (var i in test_questions) {
       if (test_questions[i].author === authorName) {
         author.addQuestion(test_questions[i]);
       }
-    };
+    }
   });
 
   it('has 5 test_questions', function () {
@@ -19,26 +19,26 @@ describe('author Yves', function () {
 
   it('displays correctly', function () {
     var asListItem = author.asListItem();
-    asListItem.should.contain('<ul data-role=\'listview\'>');
+    asListItem.should.contain('<ul data-role="listview"');
     asListItem.should.contain('<h3>' + author.name);
-    asListItem.should.contain('<li><small>');
+    asListItem.should.contain('<li>');
   });
 });
 
 describe('authors function', function () {
   var result;
-  beforeEach(function() {
+  beforeEach(function () {
     result = authors(test_questions);
   })
 
-  it('parses the test_questions to 7 authors', function() {
+  it('parses the test_questions to 7 authors', function () {
     result.length.should.equal(7);
     for (var i in result) {
       result[i].should.be.instanceOf(Author);
     }
   });
 
-  it('contains Yves with 5 test_questions', function() {
+  it('contains Yves with 5 test_questions', function () {
     for (var i in result) {
       if (result[i].name === '@yveshanoulle') {
         result[i].questions.length.should.equal(5);
@@ -46,7 +46,7 @@ describe('authors function', function () {
     }
   });
 
-  it('sorts the authors by name', function() {
+  it('sorts the authors by name', function () {
     var lastName = "";
     for (var i in result) {
       var currentName = result[i].name;
