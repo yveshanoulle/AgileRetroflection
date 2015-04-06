4374440d337cddea881bb5c1710d246b79766cbc
leider <derleider@web.de>
2015-04-06 20:14:45 +0200

    sorting questions by id after retrieval

--- server/retrieve-questions.js
+++ server/retrieve-questions.js
@@ -9,12 +9,14 @@ var async = require('async');
 var nconf = require('nconf');
 nconf.argv();
 nconf.env();
-nconf.file({ file: 'config/emailAndKey.json' });
+nconf.file({file: 'config/emailAndKey.json'});
 
 function loadSheet(worksheetId, idCol, questionCol, authorCol, dateCol, callback) {
   var key = nconf.get('GOOGLE_PEM');
   var email = nconf.get('GOOGLE_DEV_EMAIL');
-  if (!key || !email) { return callback(new Error()); }
+  if (!key || !email) {
+    return callback(new Error());
+  }
   Spreadsheet.load({
     spreadsheetId: '0AjzWaZIjE9dYdFg2aXdNbE5qdzlzNjBmM1pKaFJFYkE',
     worksheetId: worksheetId,
@@ -25,10 +27,14 @@ function loadSheet(worksheetId, idCol, questionCol, authorCol, dateCol, callback
     }
 
   }, function sheetReady(err, spreadsheet) {
-    if (err) { return callback(err); }
+    if (err) {
+      return callback(err);
+    }
 
     spreadsheet.receive({getValues: true}, function (err, rows) {
-      if (err) { return callback(err); }
+      if (err) {
+        return callback(err);
+      }
       callback(null, _(rows).filter(function (row) {
         return !!row[dateCol] && moment(row[dateCol], 'M/D/YYYY').add(1, 'days').isBefore(moment());
       }).map(function (row) {
@@ -37,7 +43,9 @@ function loadSheet(worksheetId, idCol, questionCol, authorCol, dateCol, callback
         questionrow.question = row[questionCol];
         questionrow.author = row[authorCol];
         return questionrow;
-      }).filter(function (row) { return !!row.author && row.author.match(/^@/); }).value());
+      }).filter(function (row) {
+        return !!row.author && row.author.match(/^@/);
+      }).value());
     });
   });
 }
@@ -45,18 +53,37 @@ function loadSheet(worksheetId, idCol, questionCol, authorCol, dateCol, callback
 module.exports = function () {
   async.parallel(
     [
-      function (callback) { loadSheet('od9', '2', '4', '5', '9', callback); },
-      function (callback) { loadSheet('od8', '2', '4', '5', '9', callback); },
-      function (callback) { loadSheet('odb', '2', '4', '5', '9', callback); },
-      function (callback) { loadSheet('ocx', '1', '3', '4', '8', callback); },
-      function (callback) { loadSheet('od6', '1', '3', '4', '7', callback); },
-      function (callback) { loadSheet('od0', '1', '3', '4', '7', callback); }
+      function (callback) {
+        loadSheet('od9', '2', '4', '5', '9', callback);
+      },
+      function (callback) {
+        loadSheet('od8', '2', '4', '5', '9', callback);
+      },
+      function (callback) {
+        loadSheet('odb', '2', '4', '5', '9', callback);
+      },
+      function (callback) {
+        loadSheet('ocx', '1', '3', '4', '8', callback);
+      },
+      function (callback) {
+        loadSheet('od6', '1', '3', '4', '7', callback);
+      },
+      function (callback) {
+        loadSheet('od0', '1', '3', '4', '7', callback);
+      }
     ],
     function (err, questions) {
-      if (err) { return err; }
-      fs.writeFile('public/questions.json', JSON.stringify(_.flatten(questions)), function (err) {
-        if (err) { return console.log(err); }
-        console.log("questions updated");
+      if (err) {
+        return err;
+      }
+      var sortedQuestions = _.flatten(questions).sort(function (question1, question2) {
+        return parseInt(question1.id, 10) - parseInt(question2.id, 10);
+      });
+      fs.writeFile('public/questions.json', JSON.stringify(sortedQuestions), function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('questions updated');
       });
     }
   );
